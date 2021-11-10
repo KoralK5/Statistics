983f2e0ad7f82ac33f25c53c6ee9232290ac5734
Koral <62809012+KoralK5@users.noreply.github.com>
2021-11-10 12:17:00 -0500

    Create statistics.py

---
+++ statistics.py
@@ -0,0 +1,51 @@
+def population(x):
+	return len(x)
+
+def variance(x):
+	return (sum([(i-sum(x)/population(x))**2 for i in x])/population(x))
+
+def standard_deviation(x):
+	return variance(x)**(1/2)
+
+def frequency(x):
+	freqs = {}
+	for i in x:
+		if i in freqs:
+			freqs[i] += 1
+		else:
+			freqs[i] = 1
+	return freqs
+
+def mean(x):
+	return sum(x)/population(x)
+
+def median(x):
+	return x[population(x)//2] if population(x)%2==1 else (x[population(x)//2-1] + x[population(x)//2])/2
+
+def mode(x):
+	freqs = frequency(x)
+	return max(freqs, key=freqs.get)
+
+def data_range(x):
+	return max(x) - min(x)
+
+def quartiles(x, q):
+	x.sort()
+	return [population(x)*(i/q) for i in range(1, q+1)]
+
+def quartile_split(x, q):
+	qs = quartiles(x, q)
+	print(qs)
+	return [x[:int(qs[0])]] + [x[int(qs[i]):int(qs[i+1])] for i in range(q-1)]
+
+def class_width(x, i):
+	return max(x[i]) - min(x[i])
+
+def class_midpoint(x, i):
+	return median(x[i])
+
+def class_frequency(x, i):
+	return sum(frequency(x[i]).values())
+
+def class_mean(x):
+	return sum([class_midpoint(x, i) for i in range(len(x))])/sum([class_frequency(x, i) for i in range(len(x))])
